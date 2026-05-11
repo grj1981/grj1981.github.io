e0cd4e6cc2fb972596210e596d74a654a6092554
grj1981 <271623821@qq.com>
2026-05-11 18:39:08 +0800

    Site updated: 2026-05-11 18:38:24

---
+++ sw.js
@@ -0,0 +1,22 @@
+var CACHE_NAME = 'bytefisher-v1';
+var urlsToCache = [
+  '/',
+  '/css/main.css',
+  '/js/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
